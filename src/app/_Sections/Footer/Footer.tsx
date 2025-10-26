5303a7bc6c733498ed5a22d7ca3c1aebb061462a
john-ryabikov <zhenya.ryabikov@mail.ru>
2025-10-26 20:58:11 +0300

    v1.0.1

--- src/app/_Sections/Footer/Footer.tsx
+++ src/app/_Sections/Footer/Footer.tsx
@@ -10,7 +10,7 @@ export default function Footer({ typedClass }: Props) {
   return (
     <footer className={`${typedClass}`}>
       <Container typedClass={`${typedClass}__cont wrapper`}>
-        <img className={`${typedClass}__img`} src="/img/footer/logo-footer.png" alt="Paradise Travel"/>
+        <img className={`${typedClass}__img`} src="/img/Footer/logo-footer.png" alt="Paradise Travel"/>
         <p className={`${typedClass}__info`}>
           <span className={`${typedClass}__author`}>Дизайн и разработка сайта - <a className={`${typedClass}__author-link`} href="https://just-site.ru" target="_blank" rel="nofollow">Just-site.ru</a></span>
           <span className={`${typedClass}__copyrights`}>&copy;Paradise Travel 2012-{new Date().getFullYear()}. <a className={`${typedClass}__copyrights-link`} href="mailto:paradisetravel.el@gmail.com" rel="nofollow">paradisetravel.el@gmail.com</a></span>
