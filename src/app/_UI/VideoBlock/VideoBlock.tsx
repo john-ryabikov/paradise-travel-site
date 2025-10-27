443c9bb89c6a355a667abdc1ddbf654263481093
John <64689339+john-ryabikov@users.noreply.github.com>
2025-10-27 16:05:28 +0300

    v1.0.16

--- src/app/_UI/VideoBlock/VideoBlock.tsx
+++ src/app/_UI/VideoBlock/VideoBlock.tsx
@@ -21,33 +21,33 @@ export default function VideoBlock() {
     <>
       {hasWindow && 
         <div className='video-block'>
-            <ReactPlayer
-              url={"/img/Section-1/video-fon.webm"}
-              width="100%"
-              height="100%"
-              style={{ width: '100%', height: '100%' }}
-              playing
-              playsInline
-              loop
-              preload={true}
-              muted
-              config={{
-              file: {
-                attributes: {
-                  style: {
-                    position: 'absolute',
-                    top: '50%',
-                    left: '50%',
-                    transform: 'translate(-50%, -50%)',
-                    minWidth: '100%',
-                    minHeight: '100%',
-                    objectFit: 'cover'
+            {!isMobile && (
+              <ReactPlayer
+                url={"/img/Section-1/video-fon.webm"}
+                width="100%"
+                height="100%"
+                style={{ width: '100%', height: '100%' }}
+                playing
+                playsInline
+                loop
+                muted
+                config={{
+                file: {
+                  attributes: {
+                    style: {
+                      position: 'absolute',
+                      top: '50%',
+                      left: '50%',
+                      transform: 'translate(-50%, -50%)',
+                      minWidth: '100%',
+                      minHeight: '100%',
+                      objectFit: 'cover'
+                    }
                   }
                 }
-              }
-             }}
-             light={!isMobile ? "/img/Section-1/video-fon-fallback_screen.jpg" : "/img/Section-1/video-fon-fallback_mob.jpg"}
-            />
+               }}
+              />
+            )
         </div> 
       }
     </>
