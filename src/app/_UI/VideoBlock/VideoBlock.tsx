f3724e143ad2ec736a06e539bf7f2cebc1d2f827
John <64689339+john-ryabikov@users.noreply.github.com>
2025-10-27 12:45:48 +0300

    v1.0.10

--- src/app/_UI/VideoBlock/VideoBlock.tsx
+++ src/app/_UI/VideoBlock/VideoBlock.tsx
@@ -25,12 +25,27 @@ export default function VideoBlock() {
               url={"/img/Section-1/video-fon.webm"}
               width="100%"
               height="100%"
-              style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+              style={{ width: '100%', height: '100%' }}
               playing
               playsInline
               loop
               preload={true}
               muted
+              config={{
+              file: {
+                attributes: {
+                  style: {
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    minWidth: '100%',
+                    minHeight: '100%',
+                    objectFit: 'cover'
+                  }
+                }
+              }
+            }}
             />
         </div> 
       }
