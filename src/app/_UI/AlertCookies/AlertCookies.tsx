4dea8022c16a36399b5b74954d67b2236c4ff43f
John <64689339+john-ryabikov@users.noreply.github.com>
2025-10-28 11:34:08 +0300

    v1.1.16

--- src/app/_UI/AlertCookies/AlertCookies.tsx
+++ src/app/_UI/AlertCookies/AlertCookies.tsx
@@ -7,14 +7,18 @@ export default function AlertCookies() {
     const { showAlertCookies, closeAlertCookies } = useAlertCookies()
 
     return (
-        <p className={`alert-cookies ${!showAlertCookies ? "not-active" : ""}`}>
-            <span className="alert-cookies__text">
-                Этот сайт использует файлы cookie и другие технологии для сбора аналитических данных. Это помогает нам улучшать работу сайта и показывать Вам релевантный контент.
-                Продолжая использовать сайт, Вы соглашаетесь с этим.
-            </span>
-            <button onClick={closeAlertCookies}>
-                <span>ОК</span>
-            </button>
-        </p>
+        <>
+            {showAlertCookies && (
+                <p className={`alert-cookies ${!showAlertCookies ? "not-active" : ""}`}>
+                    <span className="alert-cookies__text">
+                        Этот сайт использует файлы cookie и другие технологии для сбора аналитических данных. Это помогает нам улучшать работу сайта и показывать Вам релевантный контент.
+                        Продолжая использовать сайт, Вы соглашаетесь с этим.
+                    </span>
+                    <button onClick={closeAlertCookies}>
+                        <span>ОК</span>
+                    </button>
+                </p>
+            )}
+        </>
     )
 }
