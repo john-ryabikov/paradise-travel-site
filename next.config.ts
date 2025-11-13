2b8a14318e18da6ed80740256f5275d2ed9e16e4
John <64689339+john-ryabikov@users.noreply.github.com>
2025-11-13 08:46:24 +0300

    v1.2.2

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   reactStrictMode: true,
-  output: 'export'
+  output: 'export',
+  trailingSlash: true
 };
 
 export default nextConfig;
