40fef875492e0725c2238a4e56f0b53173da83be
John <64689339+john-ryabikov@users.noreply.github.com>
2025-11-14 09:51:06 +0300

    v1.2.24

--- src/app/[...not_found]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { notFound } from 'next/navigation';
-
-export default function CatchAllPage() {
-  notFound()
-}
-
-export async function generateStaticParams() {
-  return [{ not_found: [''] }];
-}
