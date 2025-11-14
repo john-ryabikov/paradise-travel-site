9e3041b7e718ba3584402b5f69c347d9b32ca8f9
John <64689339+john-ryabikov@users.noreply.github.com>
2025-11-14 09:07:10 +0300

    v1.2.18

--- src/app/[...not_found]/page.tsx
+++ src/app/[...not_found]/page.tsx
@@ -1,9 +1,5 @@
 import { notFound } from 'next/navigation';
 
 export default function CatchAllPage() {
-  notFound();
-}
-
-export async function generateStaticParams() {
-  return [];
+  notFound()
 }
