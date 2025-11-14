f592de33d80681622124bea3c39428c801bfc597
John <64689339+john-ryabikov@users.noreply.github.com>
2025-11-14 10:00:41 +0300

    v1.2.25

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
-  distDir: 'out'
+  distDir: 'out',
+  skipTrailingSlashRedirect: true,
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/404',
+        has: [{ type: 'header', key: 'x-not-found', value: 'true' }]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
